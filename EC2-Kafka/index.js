8af97e84ee7525083a9a2ff79e21a6f2b2ea53fd
Daksh Sawhney <darkstardaksh@gmail.com>
2025-09-06 13:34:21 +0530

    Completed Kinesis Aiven EC2 Server

--- EC2-Kafka/index.js
+++ EC2-Kafka/index.js
@@ -3,6 +3,12 @@ const path = require('path')    // used to join path
 const fs = require('fs')
 const express = require('express')
 
+require('dotenv').config();
+
+console.log(process.env.KAFKA_USERNAME)
+console.log(process.env.KAFKA_PASSWORD)
+console.log(process.env.KAFKA_BROKER)
+
 // Create Kafka Instance
 const kafka = new Kafka({
     brokers: [process.env.KAFKA_BROKER],
@@ -23,16 +29,31 @@ const PORT = 10000;
 
 app.use(express.json());
 
-// Creating Kafka Producer
 const producer = kafka.producer();
-console.log('Connected to kafka producer')
+
+const startServer = async() => {
+    try {
+        // Connect Kafka Producer
+        await producer.connect();
+        console.log('Connected to kafka producer')
+
+        // Start server after producer is connected
+        app.listen(PORT, () => {
+            console.log(`Listening on port: ${PORT}`)
+        })
+    } catch (error) {
+        console.error('Error Occured', error);
+        process.exit(1);        
+    }
+}
+
+startServer();
 
 // Fetching Data on /data route
 app.post('/data', async(req,res) => {
     try {
         const { data } = req.body;
         if(!data || data.length == 0) return res.status(500).send({ status: 'No Data Recieved' })
-
 
         // Store all messages into list
         const kafkaMessages = data.map(record => ({
@@ -41,21 +62,17 @@ app.post('/data', async(req,res) => {
         }))
 
         // Send this list to kafka topic
-        producer.send({
+        await producer.send({
             topic: `aiven-kafka`,
             messages: kafkaMessages
         })
         console.log('Sent logs to topic `aiven-kafka`')
 
 
-        res.status(200).send({status: 'Recvd and Send to Kafka', count: kafkaMessages.length()})
+        res.status(200).send({status: 'Recvd and Send to Kafka', count: kafkaMessages.length})
     } catch (error) {
         console.error('Error occured:', error)   
         res.status(500).send({status: 'Failed Sending to Kafka'})
     }
 })
 
-
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
