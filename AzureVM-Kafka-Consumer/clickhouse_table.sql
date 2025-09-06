4b9f7bd607c3100c025a96541a2fe11f35057bc6
Daksh Sawhney <darkstardaksh@gmail.com>
2025-09-06 22:41:08 +0530

    Updated ClickHouse SQL Table

--- AzureVM-Kafka-Consumer/clickhouse_table.sql
+++ AzureVM-Kafka-Consumer/clickhouse_table.sql
@@ -1,8 +1,24 @@
-CREATE TABLE log_streams (
+CREATE TABLE log_stream (
     event_id UUID,
-    timestamp DateTime MATERIALIZED now(),
+    timestamp DateTime,
     record String,
     metadata Nullable(String)
 )
-ENGINE = MergeTree PARTITION BY toYYYYMM(timestamp)
+ENGINE = MergeTree 
+PARTITION BY toYYYYMM(timestamp)
 ORDER BY (timestamp, event_id)
+
+-- New Table
+CREATE TABLE cloudfront_logs (
+    event_id UUID,
+    timestamp DateTime,
+    ip String,
+    status UInt16,
+    method String,
+    url_path String,
+    edge_location String,
+    user_agent String
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(timestamp)
+ORDER BY (timestamp, event_id);
