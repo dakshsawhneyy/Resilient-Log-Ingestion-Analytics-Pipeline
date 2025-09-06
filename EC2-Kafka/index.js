61ea9e208f7799fd97b35546598b0e5b256c5727
Daksh Sawhney <darkstardaksh@gmail.com>
2025-09-06 12:26:35 +0530

    Completed EC2 Server

--- EC2-Kafka/index.js
+++ EC2-Kafka/index.js
@@ -25,13 +25,34 @@ app.use(express.json());
 
 // Creating Kafka Producer
 const producer = kafka.producer();
-
+console.log('Connected to kafka producer')
 
 // Fetching Data on /data route
-app.post('/data', (req,res) => {
-    const { data } = req.body;
-    console.log(data);
-    res.send({status: 'Recieved'})
+app.post('/data', async(req,res) => {
+    try {
+        const { data } = req.body;
+        if(!data || data.length == 0) return res.status(500).send({ status: 'No Data Recieved' })
+
+
+        // Store all messages into list
+        const kafkaMessages = data.map(record => ({
+            key: record.id || 'default',
+            value: JSON.stringify(record),
+        }))
+
+        // Send this list to kafka topic
+        producer.send({
+            topic: `aiven-kafka`,
+            messages: kafkaMessages
+        })
+        console.log('Sent logs to topic `aiven-kafka`')
+
+
+        res.status(200).send({status: 'Recvd and Send to Kafka', count: kafkaMessages.length()})
+    } catch (error) {
+        console.error('Error occured:', error)   
+        res.status(500).send({status: 'Failed Sending to Kafka'})
+    }
 })
 
 
